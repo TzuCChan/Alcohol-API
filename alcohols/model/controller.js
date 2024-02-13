a463b5d8795d9701dc726c476937c03f847f7a35
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-12 19:27:29 -0500

    controller js 1

---
+++ alcohols/model/controller.js
@@ -0,0 +1,27 @@
+import Cocktail from "./cocktailmodel.js"
+
+export const getCocktails = async (req, res) => {
+  try {
+    const cocktails = await Cocktail.find()
+    res.json(cocktails)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message })
+  }
+}
+
+export const getCocktail = async (req, res) => {
+  try {
+    const { id } = req.params
+    const cocktails = await Cocktail.findById(id)
+
+    if (cocktails) {
+      return res.json(cocktails)
+    }
+
+    res.status(400).json({ message: "Cocktail not found!" })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: error.message })
+  }
+};
