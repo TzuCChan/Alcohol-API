aedbc23c48efc334207928a9c850320b6b91a5a3
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-21 16:33:36 -0500

    cocktail name, glass

--- alcohols/model/router.js
+++ alcohols/model/router.js
@@ -4,5 +4,7 @@ import * as controllers from "./controller.js"
 const router = Router()
 router.get("/cocktails", controllers.getCocktails)
 router.get("/cocktails/:id", controllers.getCocktail)
+router.get("/cocktails/:name", controllers.getCocktailByName)
+router.get("/cocktails/:glass", controllers.getCocktailsByGlass)
 
 export default router
