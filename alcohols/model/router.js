fb7a6d77a31ef4ac332bcb1b8c9a183ec78bc2a2
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-21 16:05:05 -0500

    router.get id

--- alcohols/model/router.js
+++ alcohols/model/router.js
@@ -2,5 +2,7 @@ import { Router } from 'express'
 import * as controllers from "./controller.js"
 
 const router = Router()
+router.get("/cocktails", controllers.getCocktails)
+router.get("/cocktails/:id", controllers.getCocktail)
 
 export default router
