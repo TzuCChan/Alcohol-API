77ece531fa81298b5d578f32b254b47f4249fabd
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-21 16:46:18 -0500

    cocktail id, id

--- alcohols/model/router.js
+++ alcohols/model/router.js
@@ -6,5 +6,8 @@ router.get("/cocktails", controllers.getCocktails)
 router.get("/cocktails/:id", controllers.getCocktail)
 router.get("/cocktails/:name", controllers.getCocktailByName)
 router.get("/cocktails/:glass", controllers.getCocktailsByGlass)
+router.post("/cocktails", controllers.createCocktail)
+router.put("/cocktails/:id", controllers.updateCocktail)
+router.delete("/cocktails/:id", controllers.deleteCocktail)
 
 export default router
