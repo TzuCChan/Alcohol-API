0f579b90e49acfbfba1d27a15f6713d35c5b41dc
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-22 15:32:49 -0500

    const congodb_uri

--- connection.js
+++ connection.js
@@ -3,4 +3,7 @@ const DATABASE_NAME = "cocktailAPI"
 
 let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 
+const MONGODB_URI =
+  process.env.PROD_MONGODB || `mongodb://127.0.0.1:27017/${DATABASE_NAME}`
+
 export default mongoose.connection
