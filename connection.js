53e781ff423ff6a6fea3c78a05dd88174d78c626
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-23 16:12:15 -0500

    mongoose.set

--- connection.js
+++ connection.js
@@ -6,4 +6,8 @@ let mongooseConfig = { useNewUrlParser: true, useUnifiedTopology: true }
 const MONGODB_URI =
   process.env.PROD_MONGODB || `mongodb://127.0.0.1:27017/${DATABASE_NAME}`
 
+mongoose.set("debug", true)
+
+mongoose.set("returnOriginal", false)
+
 export default mongoose.connection
