f60cf349b7462a1d9c69082b577ad83a16417568
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-28 18:36:27 -0500

    const router

--- mainrouter.js
+++ mainrouter.js
@@ -1,4 +1,10 @@
 import { Router } from "express"
 import routes from "./cocktails/model/router.js"
 
+const router = Router()
+
+router.get("/", (request, response) => response.send("This is the api root!"))
+
+router.use("/", routes)
+
 export default router
