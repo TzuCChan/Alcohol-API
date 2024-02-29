5fa96b09b93bd948ddee47fb8c75d7d3e7176dac
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-29 17:32:36 -0500

    const insertData

--- seed.js
+++ seed.js
@@ -1,3 +1,11 @@
 import db from "./connection.js"
 import Cocktail from "./cocktails/model/cocktailmodel.js"
 import cocktails from "./seedcocktails.json" assert { type: "json" }
+
+const insertData = async () => {
+  await db.dropDatabase()
+
+  await Cocktail.insertMany(cocktails.drinks[0])
+
+  db.close()
+}
