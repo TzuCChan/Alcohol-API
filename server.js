033cc138b38f8eafe70faad2dfd8de23580d40ec
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-03-03 17:15:50 -0500

    db.on connected

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use("/", router)
+
+db.on("connected", () => {
+  console.clear()
+  console.log("Connected to MongoDB!")
+  app.listen(PORT, () => {
+    process.env.NODE_ENV === "production"
+      ? console.log(`Express server running in production on port ${PORT}\n\n`)
+      : console.log(
+          `Express server running in development on: http://localhost:${PORT}`
+        )
+  })
+})
